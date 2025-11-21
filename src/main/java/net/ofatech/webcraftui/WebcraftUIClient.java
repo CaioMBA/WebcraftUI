0ec5ee18a9eb6288e434961334f2811d16e3448a
Caio Araujo <caiombaraujo@gmail.com>
2025-11-21 20:02:52 -0300

    start removing template bs

--- src/main/java/net/ofatech/webcraftui/WebcraftUIClient.java
+++ src/main/java/net/ofatech/webcraftui/WebcraftUIClient.java
@@ -1,6 +1,5 @@
 package net.ofatech.webcraftui;
 
-import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
@@ -11,21 +10,15 @@ import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 // This class will not load on dedicated servers. Accessing client side code from here is safe.
-@Mod(value = WebcraftUI.MODID, dist = Dist.CLIENT)
+@Mod(value = WebcraftUI.MOD_ID, dist = Dist.CLIENT)
 // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-@EventBusSubscriber(modid = WebcraftUI.MODID, value = Dist.CLIENT)
+@EventBusSubscriber(modid = WebcraftUI.MOD_ID, value = Dist.CLIENT)
 public class WebcraftUIClient {
     public WebcraftUIClient(ModContainer container) {
-        // Allows NeoForge to create a config screen for this mod's configs.
-        // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
-        // Do not forget to add translations for your config options to the en_us.json file.
         container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
     }
 
     @SubscribeEvent
     static void onClientSetup(FMLClientSetupEvent event) {
-        // Some client setup code
-        WebcraftUI.LOGGER.info("HELLO FROM CLIENT SETUP");
-        WebcraftUI.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
     }
 }
